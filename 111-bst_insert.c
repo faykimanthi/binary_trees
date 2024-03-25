48f4b9d0fe3bd1d82c6d3577bedd41c54846b30f
Faith <faykimanthi@gmail.com>
2024-03-25 11:51:59 -0700

    binary trees

---
+++ 111-bst_insert.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - This inserts a value in a Binary Search Tree.
+ * @tree: Represents a double pointer to the root node of the BST to insert the value.
+ * @value: This is the value to store in the node to be inserted.
+ *
+ * Return: A pointer to the created node, or NULL on failure.
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *current, *new;
+
+	if (tree != NULL)
+	{
+		current = *tree;
+		if (current == NULL)
+		{
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+		if (value < current->n)
+		{
+			if (current->left != NULL)
+				return (bst_insert(&current->left, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->left = new);
+		}
+		if (value > current->n)
+		{
+			if (current->right != NULL)
+				return (bst_insert(&current->right, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->right = new);
+		}
+	}
+
+	return (NULL);
+}
---
+++ 112-array_to_bst.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - This builds a Binary Search Tree from an array.
+ * @array: Represents a pointer to the first element of the array to be converted.
+ * @size: This is the number of element in the array
+ *
+ * Return: A pointer to the root node of the created BST, or NULL on failure.
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *tree = NULL;
+	size_t index, jndex;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+	{
+		for (jndex = 0; jndex < index; jndex++)
+		{
+			if (array[jndex] == array[index])
+				break;
+		}
+		if (jndex == index)
+		{
+			if (bst_insert(&tree, array[index]) == NULL)
+				return (NULL);
+		}
+	}
+
+	return (tree);
+}
