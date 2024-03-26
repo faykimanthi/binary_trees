0cc5221be97c39ab1d0d38c4d1bd78e72de48385
Faith <faykimanthi@gmail.com>
2024-03-26 09:14:57 -0700

    binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,17 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t hlf = 0, hrg = 0;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (!tree)
-		return (0);
-
-	if (tree->left)
-		hlf = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		hrg = 1 + binary_tree_height(tree->right);
-
-	if (hlf > hrg)
-		return (hlf);
-	return (hrg);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
